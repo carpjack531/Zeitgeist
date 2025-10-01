560f1e6c0fbb30e4d97f56754be3faf520c4f0d3
K1NGJAK3 <43651411+K1NGJAK3@users.noreply.github.com>
2025-10-01 16:52:22 -0400

    Updated Login Page to Current Version

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,26 @@
 import Header from '../comps/Header';
+import test from "../assets/test.jpg"
+import React, { useState, useEffect } from 'react';
+
 const LoginPage = () =>{
+    const [screenSize, setScreenSize] = useState(window.innerWidth)
     return (
-    <div className="flex flex-col content-center items-center font-arimo">
-            <div className="p-5 m-5 text-1xl font-mono rounded-lg border-2">
-                <p className="text-4xl mb-9">Sign In</p>
-                <p> Username</p>
-                <input type="email"  className="rounded-sm bg-gray-300 mb-5"/>
-                <p>Password</p>
-                <input type="password" className="rounded-sm bg-gray-300"/>
+    //Just a wrapper for components that maintains flex flow
+    <div className="max-w-screen-xl flex mx-20 my-10 sm:mx-0 sm:my-0 justify-center flex-1 ">
+        <div className= "flex flex-1 bg-white justify-center font-arimo"> 
+            <div className="flex flex-col mt-12 items-center">
+                <div className="mx-auto max-w-xs text-center">
+                    <h1 className="text-3xl xl:text-3xl font-bold mb-9 ">Sign In</h1>
+                    <input type="email"  className="w-full ml-2 p-2 rounded-md bg-gray-300  focus:bg-gray-50 mb-5" placeholder="Username"/>
+                    <input type="password" className="w-full ml-2 p-2 rounded-md bg-gray-300 focus:bg-gray-50 mb-5"  placeholder="Password"/>
+                    <input type="submit" className="w-1/2 mt-5 rounded-sm p-2 hover:bg-blue-400 bg-blue-300"/>
+                </div>
             </div>
+        </div>
+        <div className="flex flex-1 bg-red-500">
+            if<
+                <img src={test}></img>
+        </div>
     </div>
     )
 };
