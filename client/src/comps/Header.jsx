412e4e4b8e7d191d30d4940ea7a1888f132459cc
K1NGJAK3 <43651411+K1NGJAK3@users.noreply.github.com>
2025-10-10 19:22:33 -0400

    Restructured File System into Client/Server Subfolders

--- client/src/comps/Header.jsx
+++ client/src/comps/Header.jsx
@@ -1,6 +1,6 @@
 const Header = (props) => {
     return( 
-            <div className={`flex w-full items-start gap-5 p-3 text-2xl font-amino rounded-lg bg-${props.color}`}>
+            <div className={`flex w-full items-start gap-5 p-3 text-2xl font-amino rounded-lg bg-pastel-purple-500`}>
                 <img className="h-20 w-auto" src={props.logo}></img>
                 <h1 className="text-6xl font-bold"> {props.title} </h1> 
                     <div className="flex ml-10 gap-10">
---
+++ client/src/pages/SettingsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserHeader from '../comps/UserHeader.jsx'
+const SettingsPage = (props) => {
+
+    return (
+        <div className="flex flex-col items-start m-10 min-h-screen bg-pastel-purple-500">
+            <div className="flex flex-row bg-pastel-purple-200">
+                <h1 className="font-bold text-4xl">Settings</h1>
+            </div>
+            <p>IF WE HAD ONE!</p>
+        </div>
+    );
+};
+
+export default SettingsPage;
